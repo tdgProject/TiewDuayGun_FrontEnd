b618a7821881b1ce92b6ad77d544b860ea65162e
PongzZzZ <phopthorn.patcharoen@mail.kmutt.ac.th>
2021-05-10 21:42:23 +0700

    edit9

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,15 +1,15 @@
 <template>
 <nav class="bg-white px-8 pt-2 shadow-md">
      <div class="-mb-px flex justify-center">
-         <a class="no-underline text-teal-dark border-b-2 border-teal-dark uppercase tracking-wide font-bold text-xs py-3 mr-8" href="/">
+         <router-link class="no-underline text-teal-dark border-b-2 border-teal-dark uppercase tracking-wide font-bold text-xs py-3 mr-8" to="/">
              Home
-         </a>
-         <a class="no-underline text-grey-dark border-b-2 border-transparent uppercase tracking-wide font-bold text-xs py-3 mr-8" href="/Product">
+         </router-link>
+         <router-link class="no-underline text-grey-dark border-b-2 border-transparent uppercase tracking-wide font-bold text-xs py-3 mr-8" to="/Product">
              Products
-         </a>
-         <a class="no-underline text-grey-dark border-b-2 border-transparent uppercase tracking-wide font-bold text-xs py-3 mr-8" href="/About">
+         </router-link>
+         <router-link class="no-underline text-grey-dark border-b-2 border-transparent uppercase tracking-wide font-bold text-xs py-3 mr-8" to="/About">
               About
-         </a>
+         </router-link>
 
      </div>
 </nav>
