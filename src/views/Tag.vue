9213425a923f28e9549b091f3d62c29bda44a152
Attapon Jeamjumroensuk <attapon.pan@mail.kmutt.ac.th>
2021-09-05 22:06:46 +0700

    [Update]Tag.vue Design

--- src/views/Tag.vue
+++ src/views/Tag.vue
@@ -1,14 +1,77 @@
 <template>
-    <div>
-        
+  <div class="container-fluid pt-12">
+    <div class="w-3/4 m-auto py-12 justify-content-md-center">
+      
+      <!--Real Code-->
+      <!-- <ul>
+        <li v-for="type in types" :key="type">
+          <div class="border-b-2 border-secondary justify-content-start mx-5 pl-4 pb-2 fw-bold fs-3">{{ typename }}</div>
+          <ul class="mx-5 my-3 d-flex flex-wrap">
+            <li v-for="tag in tags" :key="tag">
+              <button class="w-32 h-12 mx-3 my-2 justify-content-center border border-secondary rounded-pill shadow-sm">{{ tag }}</button>
+            </li>
+          </ul>
+        </li>
+      </ul> --> 
+
+      <!--Example View-->
+      <ul>
+        <li>
+            <div class="d-flex justify-content-start border-b-2 border-secondary mx-5 pl-4 pb-2 fw-bold fs-3">typename</div>
+            <ul class="mx-5 my-3 d-flex flex-wrap">
+              <li><button class="w-32 h-12 mx-3 my-2 justify-content-center border border-secondary rounded-pill shadow-sm">tag</button></li>
+              <li><button class="w-32 h-12 mx-3 my-2 justify-content-center border border-secondary rounded-pill shadow-sm">tag</button></li>
+              <li><button class="w-32 h-12 mx-3 my-2 justify-content-center border border-secondary rounded-pill shadow-sm">tag</button></li>
+              <li><button class="w-32 h-12 mx-3 my-2 justify-content-center border border-secondary rounded-pill shadow-sm">tag</button></li>
+              <li><button class="w-32 h-12 mx-3 my-2 justify-content-center border border-secondary rounded-pill shadow-sm">tag</button></li>
+              <li><button class="w-32 h-12 mx-3 my-2 justify-content-center border border-secondary rounded-pill shadow-sm">tag</button></li>
+              <li><button class="w-32 h-12 mx-3 my-2 justify-content-center border border-secondary rounded-pill shadow-sm">tag</button></li>
+              <li><button class="w-32 h-12 mx-3 my-2 justify-content-center border border-secondary rounded-pill shadow-sm">tag</button></li>
+              <li><button class="w-32 h-12 mx-3 my-2 justify-content-center border border-secondary rounded-pill shadow-sm">tag</button></li>
+              <li><button class="w-32 h-12 mx-3 my-2 justify-content-center border border-secondary rounded-pill shadow-sm">tag</button></li>
+              <li><button class="w-32 h-12 mx-3 my-2 justify-content-center border border-secondary rounded-pill shadow-sm">tag</button></li>
+              <li><button class="w-32 h-12 mx-3 my-2 justify-content-center border border-secondary rounded-pill shadow-sm">tag</button></li>
+              <li><button class="w-32 h-12 mx-3 my-2 justify-content-center border border-secondary rounded-pill shadow-sm">tag</button></li>
+              <li><button class="w-32 h-12 mx-3 my-2 justify-content-center border border-secondary rounded-pill shadow-sm">tag</button></li>
+            </ul>
+        </li>
+        <li>
+          <div>
+            <div class="d-flex justify-content-start border-b-2 border-secondary mx-5 pl-4 pb-2 fw-bold fs-3">typename</div>
+            <ul class="mx-5 my-3 d-flex flex-wrap">
+              <li><button class="w-32 h-12 mx-3 my-2 justify-content-center border border-secondary rounded-pill shadow-sm">tag</button></li>
+              <li><button class="w-32 h-12 mx-3 my-2 justify-content-center border border-secondary rounded-pill shadow-sm">tag</button></li>
+              <li><button class="w-32 h-12 mx-3 my-2 justify-content-center border border-secondary rounded-pill shadow-sm">tag</button></li>
+              <li><button class="w-32 h-12 mx-3 my-2 justify-content-center border border-secondary rounded-pill shadow-sm">tag</button></li>
+            </ul>
+          </div>
+        </li>
+        <li>
+          <div>
+            <div class="d-flex justify-content-start border-b-2 border-secondary mx-5 pl-4 pb-2 fw-bold fs-3">typename</div>
+            <ul class="mx-5 my-3 d-flex flex-wrap">
+              <li><button class="w-32 h-12 mx-3 my-2 justify-content-center border border-secondary rounded-pill shadow-sm">tag</button></li>
+              <li><button class="w-32 h-12 mx-3 my-2 justify-content-center border border-secondary rounded-pill shadow-sm">tag</button></li>
+              <li><button class="w-32 h-12 mx-3 my-2 justify-content-center border border-secondary rounded-pill shadow-sm">tag</button></li>
+              <li><button class="w-32 h-12 mx-3 my-2 justify-content-center border border-secondary rounded-pill shadow-sm">tag</button></li>
+              <li><button class="w-32 h-12 mx-3 my-2 justify-content-center border border-secondary rounded-pill shadow-sm">tag</button></li>
+              <li><button class="w-32 h-12 mx-3 my-2 justify-content-center border border-secondary rounded-pill shadow-sm">tag</button></li>
+              <li><button class="w-32 h-12 mx-3 my-2 justify-content-center border border-secondary rounded-pill shadow-sm">tag</button></li>
+              <li><button class="w-32 h-12 mx-3 my-2 justify-content-center border border-secondary rounded-pill shadow-sm">tag</button></li>
+              <li><button class="w-32 h-12 mx-3 my-2 justify-content-center border border-secondary rounded-pill shadow-sm">tag</button></li>
+            </ul>
+          </div>
+        </li>
+      </ul>
     </div>
+  </div>
 </template>
 <script>
 // import { defineComponent } from '@vue/composition-api'
 
 // export default defineComponent({
 //     setup() {
-        
+
 //     },
 // })
 </script>
