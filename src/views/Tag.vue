0bd5488335624d4d0d50ac96e714f3c659007ba8
Attapon Jeamjumroensuk <attapon.pan@mail.kmutt.ac.th>
2021-09-21 16:45:27 +0700

    [Fix]Tagtype alighment

--- src/views/Tag.vue
+++ src/views/Tag.vue
@@ -3,7 +3,7 @@
     <div class="w-3/4 m-auto py-12 justify-content-md-center">
       <ul>
         <li v-for="type in types" :key="type.typeId">
-          <div class="border-b-2 border-secondary justify-content-start mx-5 pl-4 pb-2 fw-bold fs-3">{{ type.typeName }}</div>
+          <div class="border-b-2 border-secondary d-flex justify-content-start mx-5 pl-12 pb-2 fw-bold fs-3">{{ type.typeName }}</div>
           <ul class="mx-5 my-3 d-flex flex-wrap">
             <li v-for="tag in type.tag" :key="tag.tagId">
               <button class="w-32 h-12 mx-3 my-2 justify-content-center border border-secondary rounded-pill shadow-sm">{{ tag.tagName }}</button>
