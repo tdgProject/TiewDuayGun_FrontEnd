049613f43cb2298c7cb158adfeefcc300086721b
Attapon Jeamjumroensuk <attapon.pan@mail.kmutt.ac.th>
2021-10-03 17:18:22 +0700

    [Update] User Rating Star

--- src/views/Placeinfo.vue
+++ src/views/Placeinfo.vue
@@ -57,104 +57,7 @@
           <div class="h-100 p-5 bg-light border rounded-3">
             <form @submit.prevent="addReview()">
               <h3>Your Review and Rating</h3>
-              <fieldset
-                class="rating d-flex flex-row-reverse justify-content-center"
-              >
-                <input type="radio" id="star5" v-model="rating" :value="5" />
-                <label
-                  class="full m-0 p-0"
-                  for="star5"
-                  title="Awesome - 5 stars"
-                ></label>
-                <input
-                  type="radio"
-                  id="star4half"
-                  v-model="rating"
-                  :value="4.5"
-                />
-                <label
-                  class="half m-0 p-0"
-                  for="star4half"
-                  title="Pretty good - 4.5 stars"
-                ></label>
-                <input type="radio" id="star4" v-model="rating" :value="4" />
-                <label
-                  class="full m-0 p-0"
-                  for="star4"
-                  title="Pretty good - 4 stars"
-                ></label>
-                <input
-                  type="radio"
-                  id="star3half"
-                  v-model="rating"
-                  :value="3.5"
-                />
-                <label
-                  class="half m-0 p-0"
-                  for="star3half"
-                  title="Meh - 3.5 stars"
-                ></label>
-                <input type="radio" id="star3" v-model="rating" :value="3" />
-                <label
-                  class="full m-0 p-0"
-                  for="star3"
-                  title="Meh - 3 stars"
-                ></label>
-                <input
-                  type="radio"
-                  id="star2half"
-                  v-model="rating"
-                  :value="2.5"
-                />
-                <label
-                  class="half m-0 p-0"
-                  for="star2half"
-                  title="Kinda bad - 2.5 stars"
-                ></label>
-                <input type="radio" id="star2" v-model="rating" :value="2" />
-                <label
-                  class="full m-0 p-0"
-                  for="star2"
-                  title="Kinda bad - 2 stars"
-                ></label>
-                <input
-                  type="radio"
-                  id="star1half"
-                  v-model="rating"
-                  :value="1.5"
-                />
-                <label
-                  class="half m-0 p-0"
-                  for="star1half"
-                  title="Meh - 1.5 stars"
-                ></label>
-                <input type="radio" id="star1" v-model="rating" :value="1" />
-                <label
-                  class="full m-0 p-0"
-                  for="star1"
-                  title="Sucks big time - 1 star"
-                ></label>
-                <input
-                  type="radio"
-                  id="starhalf"
-                  v-model="rating"
-                  :value="0.5"
-                />
-                <label
-                  class="half m-0 p-0"
-                  for="starhalf"
-                  title="Sucks big time - 0.5 stars"
-                ></label>
-                <input
-                  type="radio"
-                  class="reset-option"
-                  v-model="rating"
-                  :value="reset"
-                />
-                <span class="h-auto display-6 d-flex align-items-center mx-2">{{
-                  rating
-                }}</span>
-              </fieldset>
+              <Star :rating="rating" @selected="updateStar"/>
 
               <div class="form-group">
                 <textarea
@@ -207,7 +110,8 @@
                 :key="review.userId"
               >
                 <div v-if="edit == true">
-                  <div class="d-flex pt-4 pb-3 px-5">
+                  <div class="d-flex pt-4 pb-3 px-5 justify-content-between">
+                    <div class="d-flex">
                     <img
                       src="https://i.imgur.com/Ur43esv.jpg"
                       alt="user"
@@ -218,137 +122,11 @@
                     <h6 class="font-medium font-bold mt-4 ml-4">
                       {{ review.user.username }}
                     </h6>
-                    <!--Star-->
-                    <fieldset
-                      class="
-                        rating
-                        d-flex
-                        flex-row-reverse
-                        justify-content-center
-                      "
-                    >
-                      <input
-                        type="radio"
-                        id="s5"
-                        v-model="uRating"
-                        :value="5"
-                      />
-                      <label
-                        class="full m-0 p-0"
-                        for="s5"
-                        title="Awesome - 5 stars"
-                      ></label>
-                      <input
-                        type="radio"
-                        id="s4half"
-                        v-model="uRating"
-                        :value="4.5"
-                      />
-                      <label
-                        class="half m-0 p-0"
-                        for="s4half"
-                        title="Pretty good - 4.5 stars"
-                      ></label>
-                      <input
-                        type="radio"
-                        id="s4"
-                        v-model="uRating"
-                        :value="4"
-                      />
-                      <label
-                        class="full m-0 p-0"
-                        for="s4"
-                        title="Pretty good - 4 stars"
-                      ></label>
-                      <input
-                        type="radio"
-                        id="s3half"
-                        v-model="uRating"
-                        :value="3.5"
-                      />
-                      <label
-                        class="half m-0 p-0"
-                        for="s3half"
-                        title="Meh - 3.5 stars"
-                      ></label>
-                      <input
-                        type="radio"
-                        id="s3"
-                        v-model="uRating"
-                        :value="3"
-                      />
-                      <label
-                        class="full m-0 p-0"
-                        for="s3"
-                        title="Meh - 3 stars"
-                      ></label>
-                      <input
-                        type="radio"
-                        id="s2half"
-                        v-model="uRating"
-                        :value="2.5"
-                      />
-                      <label
-                        class="half m-0 p-0"
-                        for="s2half"
-                        title="Kinda bad - 2.5 stars"
-                      ></label>
-                      <input
-                        type="radio"
-                        id="s2"
-                        v-model="uRating"
-                        :value="2"
-                      />
-                      <label
-                        class="full m-0 p-0"
-                        for="s2"
-                        title="Kinda bad - 2 stars"
-                      ></label>
-                      <input
-                        type="radio"
-                        id="s1half"
-                        v-model="uRating"
-                        :value="1.5"
-                      />
-                      <label
-                        class="half m-0 p-0"
-                        for="s1half"
-                        title="Meh - 1.5 stars"
-                      ></label>
-                      <input
-                        type="radio"
-                        id="s1"
-                        v-model="uRating"
-                        :value="1"
-                      />
-                      <label
-                        class="full m-0 p-0"
-                        for="s1"
-                        title="Sucks big time - 1 star"
-                      ></label>
-                      <input
-                        type="radio"
-                        id="shalf"
-                        v-model="uRating"
-                        :value="0.5"
-                      />
-                      <label
-                        class="half m-0 p-0"
-                        for="shalf"
-                        title="Sucks big time - 0.5 stars"
-                      ></label>
-                      <input
-                        type="radio"
-                        class="reset-option"
-                        v-model="uRating"
-                        :value="reset"
-                      />
-                      <span
-                        class="h-auto display-6 d-flex align-items-center mx-2"
-                        >{{ uRating }}</span
-                      >
-                    </fieldset>
-                    <!--Star-->
+                    </div>
+                    <div class="w-1/6">
+                      <label for="StarRange" class="container text-center">{{uRating}}</label>
+                      <input type="range" class="form-range" min="0" max="5" step="0.5" v-model="uRating" id="StarRange">
+                    </div>
                   </div>
                   <div class="m-auto w-5/6">
                     <textarea
@@ -488,73 +266,17 @@
     <footer class="pt-4 mt-4 text-muted border-top"></footer>
   </div>
 </template>
-<style scoped>
-@import url(https://netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css);
-.bd-placeholder-img {
-  font-size: 1.125rem;
-  text-anchor: middle;
-  -webkit-user-select: none;
-  -moz-user-select: none;
-  user-select: none;
-}
-@media (min-width: 768px) {
-  .bd-placeholder-img-lg {
-    font-size: 3.5rem;
-  }
-}
-.rating > [id^="star"] {
-  display: none;
-}
-.rating > [id^="s"] {
-  display: none;
-}
-.rating > label:before {
-  margin: 5px;
-  font-size: 2.25em;
-  font-family: FontAwesome;
-  display: inline-block;
-  content: "\f005";
-}
-.rating > .half:before {
-  content: "\f089";
-  position: absolute;
-}
-.rating > label {
-  color: #ddd;
-  float: right;
-}
-.rating > [id^="star"]:checked ~ label,
-.rating > [id^="s"]:checked ~ label,
-.rating:not(:checked) > label:hover,
-.rating:not(:checked) > label:hover ~ label {
-  color: #ffd700;
-}
-.rating > [id^="star"]:checked + label:hover,
-.rating > [id^="star"]:checked ~ label:hover,
-.rating > [id^="s"]:checked + label:hover,
-.rating > [id^="s"]:checked ~ label:hover,
-.rating > label:hover ~ [id^="star"]:checked ~ label,
-.rating > [id^="star"]:checked ~ label:hover ~ label {
-  color: #ffed85;
-}
-.rating > [id^="s"]:checked ~ label:hover ~ label {
-  color: #ffed85;
-}
-.reset-option {
-  display: none;
-}
-.reset-button {
-  margin: 6px 12px;
-  background-color: rgb(255, 255, 255);
-  text-transform: uppercase;
-}
-</style>
 <script>
 import { computed } from "vue";
 import { useStore } from "vuex";
+import Star from "../component/Star.vue"
 export default {
   name: "Tag",
   props: ["pid"],
+  components : {
+    Star
+  },
+
   data() {
     return {
       rating: 0,
@@ -566,7 +288,6 @@ export default {
       
     };
   },
-  components: {},
   methods: {
     // validateReview() {
     //   this.ErrorReview = this.review === "" ? true : false;
@@ -621,7 +342,10 @@ export default {
       let floor= Math.floor(rating);
       let half = rating-floor;
       return [floor,Math.ceil(half)];
-    }
+    },
+    updateStar(value){
+      this.rating = value;
+    },
   },
   setup(props) {
     const store = useStore();
